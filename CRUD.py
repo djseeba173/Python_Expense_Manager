a721584ad87f74feb98fe4115c98e9875e572be1
djseeba173 <djseeba173@gmail.com>
2022-06-14 11:11:57 -0300

    added option to order items when viewing them

--- CRUD.py
+++ CRUD.py
@@ -6,11 +6,13 @@ db = mysql.connector.connect(
     user="root",
     passwd="password",
     database="operaciones"
-    )
+)
 
 mycursor = db.cursor()
 
-clear = lambda: os.system('cls')
+
+def clear(): return os.system('cls')
+
 
 def add():
     rta = 'y'
@@ -20,52 +22,69 @@ def add():
         category = input("What category is it in? ")
         description = input("What is the description? ")
 
-        mycursor.execute("INSERT INTO Operation (amount, category, description) VALUES (%s, %s, %s)", (money, category, description))
+        mycursor.execute(
+            "INSERT INTO Operation (amount, category, description) VALUES (%s, %s, %s)", (money, category, description))
         db.commit()
         print("Operation added")
 
         rta = input("Do you want to add another operation? (y/n)")
 
+
 def view():
-    mycursor.execute("SELECT * FROM Operation")
+    order = input("Do you want to order by (amount or category)? ")
+    if order == "amount":
+        moneyOrder = input("Do you want to order by (asc or desc)? ")
+        if moneyOrder == "asc":
+            mycursor.execute("SELECT * FROM Operation ORDER BY amount ASC")
+        else:
+            mycursor.execute("SELECT * FROM Operation ORDER BY amount DESC")
+    elif order == "category":
+        categoryOrder = input("What category do you want to order by? ")
+        mycursor.execute("SELECT * FROM Operation WHERE category = %s", (categoryOrder,))
+
     for operation in mycursor:
         print(" ----------------------- ")
-        print("|",operation,"|")
+        print("|", operation, "|")
         print(" ----------------------- ")
 
     mycursor.execute("SELECT SUM(amount) FROM Operation")
     print("Total amount: ", mycursor.fetchone()[0])
 
-    
+
 def edit():
     searched = input("What do you want to edit (description)? ")
-    mycursor.execute("SELECT * FROM Operation WHERE description = %s", (searched,))
+    mycursor.execute(
+        "SELECT * FROM Operation WHERE description = %s", (searched,))
     row = mycursor.fetchone()
-    if row is not None:    
+    if row is not None:
         print("The following operation will be edited: ")
         print(" ----------------------- ")
-        print("|",row,"|")
+        print("|", row, "|")
         print(" ----------------------- ")
         newDescription = input("What is the new description? ")
         newCategory = input("What is the new category? ")
         newAmount = float(input("What is the new amount? "))
-        mycursor.execute("UPDATE Operation SET description = %s, category = %s, amount = %s WHERE description = %s", (newDescription, newCategory, newAmount, searched))
+        mycursor.execute("UPDATE Operation SET description = %s, category = %s, amount = %s WHERE description = %s",
+                         (newDescription, newCategory, newAmount, searched))
         db.commit()
     else:
         print("The description doesn't exist")
 
+
 def delete():
     searched = input("What do you want to delete (description)? ")
-    mycursor.execute("SELECT * FROM Operation WHERE description = %s", (searched,))
+    mycursor.execute(
+        "SELECT * FROM Operation WHERE description = %s", (searched,))
     row = mycursor.fetchone()
     if row is not None:
         print("The following operation will be deleted: ")
         print(" ----------------------- ")
-        print("|",row,"|")
+        print("|", row, "|")
         print(" ----------------------- ")
         rta = input("Are you sure you want to delete it? (y/n) ")
         if rta == "y":
-            mycursor.execute("DELETE FROM Operation WHERE description = %s", (searched,))
+            mycursor.execute(
+                "DELETE FROM Operation WHERE description = %s", (searched,))
             db.commit()
             print("Operation deleted")
         else:
@@ -79,19 +98,19 @@ if __name__ == "__main__":
     print("Welcome to the CRUD app")
     print("My name is Boris and I'm a software developer")
     print("I'm here to help you manage your money")
-    decision = input("What do you want to do? (a)dd, (v)iew, (e)dit, (d)elete, (q)uit: ")
+    decision = input(
+        "What do you want to do? (a)dd, (v)iew, (e)dit, (d)elete, (q)uit: ")
     while(decision != "q"):
         clear()
         if(decision == "a"):
             add()
         elif(decision == "v"):
-            view()    
+            view()
         elif(decision == "e"):
             edit()
         elif(decision == "d"):
             delete()
         else:
             print("Invalid option")
-        decision = input("What do you want to do? (a)dd, (v)iew, (e)dit, (d)elete, (q)uit: ")
-
-            
+        decision = input(
+            "What do you want to do? (a)dd, (v)iew, (e)dit, (d)elete, (q)uit: ")
